266a4cf2cce5da522cf82a501aca4f7ffc73d8a8
jack <shzhaohao@gmail.com>
2018-05-02 11:36:20 +0800

    fix print_stackframe bug

--- labcodes/lab2/kern/debug/kdebug.c
+++ labcodes/lab2/kern/debug/kdebug.c
@@ -309,17 +309,16 @@ print_stackframe(void) {
     uint32_t ebp = read_ebp();
     uint32_t eip = read_eip();
     int i = 0 ;
-    while(ebp && i < STACKFRAME_DEPTH){
+    while(ebp != 0 && i < STACKFRAME_DEPTH){
         cprintf("ebp:0x%08x eip:0x%08x ",ebp,eip);
         cprintf("args:");
-        for(int i = 2; i < 6; i ++)
-            cprintf("0x%08x ",*((uint32_t*)(ebp+i*4)));
+        for(int j = 2; j < 6; j ++)
+            cprintf("0x%08x ",*((uint32_t*)(ebp)+j));
         cprintf("\n");
         print_debuginfo(eip-1);
-        ebp = *( (uint32_t*) ebp );
-        eip = *( (uint32_t*) (ebp + 4));
+        eip = *( ((uint32_t*) (ebp) + 1));
+        ebp = *( ((uint32_t*) ebp) );
         i++;
     }
-
 }
 
